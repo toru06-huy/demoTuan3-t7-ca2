1ed4daae440d0dc2919f528802536cf4df3aacc8
NhatMinh <trinhnhatmiki@gmail.com>
2025-11-01 10:52:25 +0700

    Them ham thuong

--- Source.cpp
+++ Source.cpp
@@ -3,14 +3,17 @@
 using namespace std;
 int tong(int a, int b){
 	return a+b;
-}
+};
 int hieu(int a, int b);
 int tich(int a, int b);
-float thuong(int a, int b);
+float thuong(int a, int b){
+	return a/b;
+};
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout<<"Tong = "<<tong(1,2);
+	cout<<"Thuong = "<<thuong(4,2);
 	system("pause");
 	return 0;
 }
